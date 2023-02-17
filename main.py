de2a16ef111bd7f35b93d4ef1bfba7c058bc3a36
NoOneHardy <silas.hardegger@outlook.com>
2023-02-17 16:20:57 +0100

    _

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import pyautogui
-<<<<<<< Updated upstream
+from PIL import Image
+
 import tkinter as tk
-=======
 import time
->>>>>>> Stashed changes
-
-time.sleep(3)
-img1 = pyautogui.screenshot('screenshot.png')
-
-<<<<<<< Updated upstream
-
 
 root = tk.Tk()
 
@@ -18,8 +11,26 @@ screen_height = root.winfo_screenheight()
 
 print("Width =", screen_width)
 print("Height =", screen_height)
-=======
-koenig1 = pyautogui.center(pyautogui.locateOnScreen('karten/kreuz/koenig.png'))
 
-print(koenig1.x)
->>>>>>> Stashed changes
+def codeablaufen():
+    eckAss = Image.open('karten/eck/6.png')
+
+    height = eckAss.height * 1080 / screen_height
+    width = eckAss.width * 1920 / screen_width
+
+    eckAss.resize((width, height))
+
+    eck6 = pyautogui.center(pyautogui.locateOnScreen(eck6))
+    print(eck6.x, ",", eck6.y)
+
+    stack = pyautogui.center(pyautogui.locateOnScreen('karten/back.png'))
+
+    pyautogui.click(stack.x, stack.y)
+
+
+
+btn=tk.Button(root, text="ok", fg='blue', command=codeablaufen)
+btn.place(x=80, y=100)
+root.title('Hello Python')
+root.geometry("300x200")
+root.mainloop()
