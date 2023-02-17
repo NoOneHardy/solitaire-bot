9608ef150b4d96a51fd5f1876fb9e7a7a0ad0ff9
solitairePr0 <21v1583@bztf.ch>
2023-02-17 15:20:35 +0100

    yep

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pyautogui
+import tkinter as tk
 
 img1 = pyautogui.screenshot('screenshot.png')
+
+
+
+root = tk.Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+print("Width =", screen_width)
+print("Height =", screen_height)
