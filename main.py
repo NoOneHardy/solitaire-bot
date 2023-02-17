fcad2640e739d742f1f2e1fb6b09de68c0c65ac1
NoOneHardy <silas.hardegger@outlook.com>
2023-02-17 15:23:58 +0100

    _

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pyautogui
+<<<<<<< Updated upstream
 import tkinter as tk
+=======
+import time
+>>>>>>> Stashed changes
 
+time.sleep(3)
 img1 = pyautogui.screenshot('screenshot.png')
 
+<<<<<<< Updated upstream
 
 
 root = tk.Tk()
@@ -12,3 +18,8 @@ screen_height = root.winfo_screenheight()
 
 print("Width =", screen_width)
 print("Height =", screen_height)
+=======
+koenig1 = pyautogui.center(pyautogui.locateOnScreen('karten/kreuz/koenig.png'))
+
+print(koenig1.x)
+>>>>>>> Stashed changes
